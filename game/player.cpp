6216fc072d5f95676e521e8860d0d9fb9c20f3dc
ThodTsi <teotsi4002@gmail.com>
2024-01-08 19:26:50 +0200

    eiii

--- game/player.cpp
+++ game/player.cpp
@@ -27,6 +27,7 @@ void Player::init()
 {
 	m_pos_x = 5.0f;
 	m_pos_y = 5.0f;
+	m_width /= 2;
 
 	m_state->m_global_offset_x = m_state->getCanvasWidth() / 2.0f - m_pos_x;
 	m_state->m_global_offset_y = m_state->getCanvasHeight() / 2.0f - m_pos_y;
@@ -42,10 +43,22 @@ void Player::init()
 void Player::draw()
 {
 	graphics::drawRect(m_state->getCanvasWidth() * 0.5f, m_state->getCanvasHeight() * 0.5f, 1.0f, 1.0f, m_brush_player);
+	if (m_state->m_debug_mode) {
+		debugDraw();
+	}
+	
+}
 
-	float x = m_state->getCanvasWidth() * 0.5f;
-	float y = m_state->getCanvasHeight() * 0.5f;
-
-	if (m_state->m_debug_mode)
-		graphics::drawRect(x, y, 1.0f, 1.0f, m_brush_player_debug);
+void Player::debugDraw() {
+	graphics::Brush debug_brush;
+	SETCOLOR(debug_brush.fill_color, 1, 0.3f, 0);
+	SETCOLOR(debug_brush.outline_color, 1, 0.1f, 0);
+	debug_brush.fill_opacity = 0.1f;
+	debug_brush.outline_opacity = 1.0f;
+	graphics::drawRect(m_state->getCanvasWidth() * 0.5f, m_state->getCanvasHeight() * 0.5f, m_width, m_height, debug_brush);
+	char s[20];
+	sprintf_s(s, "(%5.2f, %5.2f", m_pos_x, m_pos_y);
+	SETCOLOR(debug_brush.fill_color, 1, 0, 0);
+	debug_brush.fill_opacity = 1.0f;
+	graphics::drawText(m_state->getCanvasWidth() * 0.5f - 0.4f, m_state->getCanvasHeight() * 0.5f - 0.6f, 0.15f, s, debug_brush);
 }
