6cdd0b14a59911036c3e97b2060d8a187aa42200
ThodTsi <teotsi4002@gmail.com>
2024-01-11 17:26:23 +0200

    Update player.h

--- game/player.h
+++ game/player.h
@@ -8,25 +8,21 @@ class Player : public GameObject, public Box
 {
 	graphics::Brush m_brush_player;
 
-	std::vector<GameObject*> m_static_object;
-	std::vector<GameObject*> m_dynamic_object;
-
-	graphics::Brush m_brush_player_debug;
-
-	const float m_gravity = 10.0f;
-	const float m_accel_vertical = 300.0f;
-	const float m_accel_horizontial = 20.0f;
+	const float m_gravity = 5.0f;
+	const float m_accel_vertical = 3.0f;
+	const float m_accel_horizontial = 5.0f;
 	const float m_max_velocity = 5.0f;
 	void movePlayer(float ms);
 
 public:
-	Player(std::string name) : GameObject(name) {}
 	float m_vx = 0.0f;
 	float m_vy = 0.0f;
+
+	Player(std::string name) : GameObject(name) {}
 	void init() override;
 	void draw() override;
 	void update(float ms) override;
 
-protected: 
+protected:
 	void debugDraw();
 };
